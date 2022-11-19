7d98d1cda4434d6ae836e3a600632e361f2844c8
mujeebabiola <abiolaabdulmujeeb@gmail.com>
2022-11-19 15:07:24 +0000

    Update 2 files

--- src/PAGES/Error404.jsx
+++ src/PAGES/Error404.jsx
@@ -4,6 +4,7 @@ export default function Error() {
     <div className="jumbotron container">
       <h1>Error 404</h1>
       <p>The page you are looking for does not exist</p>
+      <p>I am aware that the changes I make will be commited too</p>
     </div>
   );
 }
