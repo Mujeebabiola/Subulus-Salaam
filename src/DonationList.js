a2fb9036db482f7f1e458397659e76117fe7112c
Mujeebabiola <91371238+Mujeebabiola@users.noreply.github.com>
2022-12-07 21:25:22 +0800

    id were given to individual element

--- src/DonationList.js
+++ src/DonationList.js
@@ -6,21 +6,25 @@ const DonationList = [
     donations: [
       { name: "abdul", amount: "50" },
       { name: "umar", amount: "90" },
-      { name: "azeez", amount: "40" }
+      { name: "azeez", amount: "40" },
     ],
-    icon: "fa-solid fa-laptop-code fa-4x"
+    icon: "fa-solid fa-laptop-code fa-4x",
+    id: 1,
   },
+
   {
     name: "Second Donation",
     goal: 100,
     donations: [
       { name: "abdul", amount: "10" },
       { name: "umar", amount: "50" },
-      { name: "azeez", amount: "40" }
+      { name: "azeez", amount: "40" },
     ],
     aim: "The aim of this fundraiser is to not do anything lol",
-    icon: "fa-brands fa-facebook fa-4x"
+    icon: "fa-brands fa-facebook fa-4x",
+    id: 2,
   },
+
   {
     name: "First Donation",
     goal: 1000,
@@ -28,9 +32,10 @@ const DonationList = [
     donations: [
       { name: "abdul", amount: "50" },
       { name: "umar", amount: "90" },
-      { name: "azeez", amount: "40" }
+      { name: "azeez", amount: "40" },
     ],
-    icon: "fa-solid fa-laptop-code fa-4x"
-  }
+    icon: "fa-solid fa-laptop-code fa-4x",
+    id: 3,
+  },
 ];
 export default DonationList;
